3e7d103fddd73fd855d70ad2ec483a8dac176c73
thenuka99 <janith.h@creativesoftware.com>
2023-03-14 11:36:17 +0530

    leave model #1

---
+++ models/Leaves.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = new mongoose.Schema({
+    employee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reason: {
+        type: String,        
+        required: [true, "Reason cannot be empty"],
+    },
+    Type: {
+        type: Boolean,
+        required: [true, "Leave type cannot be empty"],
+    },
+    reportPerson:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    status: {
+        type: Boolean,
+        default: false,
+    },
+    canceled: {
+        type: Boolean,
+        default: false,
+    },
+    leaveFrom:{
+        type: Date,
+        required: [true, "Leave From cannot be empty"],
+    },
+    leaveTo:{
+        type: Date,
+        required: [true, "Leave To cannot be empty"],
+    },
+    isfullday: {
+        type: Boolean,
+        default: false,
+    },
+},{ timestamps: true });
+
+
+module.exports = mongoose.model('Leaves', Schema);
