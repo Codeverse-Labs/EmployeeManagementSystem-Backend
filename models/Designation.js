52ec506a9a2b71295ee2893e6fd744fbc34dabd0
thenuka99 <janithherath2@gmail.com>
2023-03-15 21:37:44 +0530

    designation model#1

---
+++ models/Designation.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name to the Designation name"],
+    },
+    addedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    updatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+},{ timestamps: true });
+
+module.exports = mongoose.model('Designation', Schema);
