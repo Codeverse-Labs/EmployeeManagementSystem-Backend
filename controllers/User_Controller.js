f004037c71985f6b3484c0f3f01aa7bc75806990
thenuka99 <janithherath2@gmail.com>
2023-03-15 21:04:36 +0530

    user controller #2

--- controllers/User_Controller.js
+++ controllers/User_Controller.js
@@ -4,15 +4,6 @@ const mongoose = require("mongoose");
 const users = require('../models/User')
 const ResponseService = require("../utils/RresponseService"); // Response service
 
-
-
-//get all
-exports.getAll=(async(req, res) => {
-  users.find((err, doc) => {
-    ResponseService.generalPayloadResponse(err, doc, res);
-  });
-});
-
 // Update
 exports.update=(async (req, res) => {
   users.findByIdAndUpdate(req.params.id, req.body, (err, doc) => {
@@ -29,7 +20,75 @@ exports.getById=(async (req, res) => {
 
 // Soft Delete
 exports.delete=(async(req, res) => {
-  users.findByIdAndUpdate(req.body.id, req.body, (err, doc) => {
+  users.findByIdAndUpdate(req.params.id, (err, doc) => {
     ResponseService.generalPayloadResponse(err, doc, res, "user deleted successfully");
   });
 });
+
+//get all
+exports.getAll=(async(req, res) => {
+  users.find((err, doc) => {
+    ResponseService.generalPayloadResponse(err, doc, res);
+  });
+});
+
+//get all by designation
+exports.getAllByDesignation = async function (req, res) {
+  const designation = req.params.designation;
+
+  // Pagination parameters
+  const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+  const page = req.query.page ? parseInt(req.query.page) - 1 : 0;
+
+
+  const totalPages = Math.ceil(await users.countDocuments({ designation: designation }) / limit);
+
+   posts.find({ categoryis: categoryId }, (err, doc) => {
+    const newPayload = {
+      docs: doc,
+      totalPages: totalPages
+    }
+    ResponseService.generalPayloadResponse(err, newPayload, res);
+  })
+    .sort({ name: -1 })
+    .skip(page * limit)
+    .limit(limit);
+}
+
+//get all by technology
+exports.getAllByDesignation = async function (req, res) {
+  const technology = req.params.technology;
+
+  // Pagination parameters
+  const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+  const page = req.query.page ? parseInt(req.query.page) - 1 : 0;
+
+
+  const totalPages = Math.ceil(await users.countDocuments({ technologies:{ $elemMatch: { item: technology}} }) / limit);
+
+   posts.find({ technologies:{ $elemMatch: { item: technology}}}, (err, doc) => {
+    const newPayload = {
+      docs: doc,
+      totalPages: totalPages
+    }
+    ResponseService.generalPayloadResponse(err, newPayload, res);
+  })
+    .sort({ name: -1 })
+    .skip(page * limit)
+    .limit(limit);
+}
+
+// search user by name
+exports.searchAllByName = async function (req, res) {
+  const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+  const page = req.query.page ? parseInt(req.query.page) : 0;
+
+  const searchTerm = req.params.searchTerm;
+  const result = posts.find({ name: { $regex: searchTerm, $options: 'i' } }, (err, doc) => {
+    ResponseService.generalPayloadResponse(err, doc, res);
+  })
+    .sort({ name: -1 })
+    .skip(page * limit)
+    .limit(limit);
+}
+
