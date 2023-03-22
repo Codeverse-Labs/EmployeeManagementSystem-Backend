3c241ffe0f20795a223222587ea30306fdccdea9
thenuka99 <janith.h@creativesoftware.com>
2023-03-22 19:22:11 +0530

    minor update

--- routes/User_Routes.js
+++ routes/User_Routes.js
@@ -19,6 +19,9 @@ router.get("/:id", async (req, res) => userController.getById(req, res));
 // Get all
 router.get("/", async (req, res) => userController.getAll(req, res));
 
+// Get all for admin
+router.get("/admin/admin", async (req, res) => userController.getAllForAdmin(req, res));
+
 // Get all by designation
 router.get("/designation/:designation", async (req, res) => userController.getAllByDesignation(req, res));
 
